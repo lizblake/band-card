6dfa19a4f0e3065aa403ab9a88bc83077b938781
Liz <elizabethblake@psu.edu>
2023-03-20 13:16:55 -0400

    added api

---
+++ src/band-list.js
@@ -0,0 +1,64 @@
+import { LitElement, html, css } from 'lit';
+import "./band-card";
+
+export class BandList extends LitElement {
+    static get tag() {
+        return 'band-list';
+    }
+    static get properties() {
+        return {
+          bands: { type: Array },
+          location: { type: String },
+        }
+    }
+
+    constructor() {
+        super();
+        this.bands = [];
+        this.location = 'State College';
+        this.updateList();
+    }
+
+    updateList() {
+        const address = new URL('../api/list', import.meta.url).href;
+        fetch(address).then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            return [];
+        })
+        .then((data) => {
+            this.bands = data;
+        });
+    }
+    
+    static get styles() {
+        return css`
+        :host {
+            display: block;
+        }
+        .wrapper {
+            border: 2px solid black;
+            display: flex;
+        }
+        .item {
+            display: inline-flex;
+        }
+    `;
+    }
+
+    render() {
+        return html`
+        <h2>${this.location}</h2>
+        <div class="wrapper">
+            ${this.bands.map(band => html`
+            <div class="item">
+                <band-card name="${band.title}" image="${band.image}" information="${band.information}" topText="${band.topText}" bottomText="${band.bottomText}" ></band-card>
+            </div>
+            `)}
+        </div>
+        `;
+    }
+}
+customElements.define(BandList.tag, BandList);
+
